c1b08346c5a3ed5b4d16e2f168a64fb625790902
Chenggong Wang <c.wang@princeton.edu>
2023-02-16 13:18:07 -0500

    update

--- fortran_test/NN_speed2.f90
+++ fortran_test/NN_speed2.f90
@@ -1,7 +1,7 @@
 module test_mod
 implicit none
 public NN_Linear_layer_type
-type :: NN_Linear_layer_type 
+type :: NN_Linear_layer_type
     real, dimension(:,:), pointer :: weight=>NULL()
     real, dimension(:),   pointer :: bias=>NULL()
 end type NN_Linear_layer_type
@@ -14,24 +14,25 @@ end type NN_FC_type
 
 type(NN_FC_type)   :: Rad_NN_FC
 
-contains 
-  
-subroutine nn_init() 
-    integer::ilayer
-    Rad_NN_FC%num_layers = 5
-    Rad_NN_FC%num_hid_nodes = 256
-    allocate(Rad_NN_FC%Layers(Rad_NN_FC%num_layers))
-    do ilayer = 1, Rad_NN_FC%num_layers
-        allocate(Rad_NN_FC%Layers(ilayer)%weight(256,256)) 
-        allocate(Rad_NN_FC%Layers(ilayer)%bias(256))  
+contains
+
+subroutine nn_init(num_layers, num_hid_nodes)
+    integer, intent(in) :: num_layers, num_hid_nodes
+    integer :: ilayer
+    Rad_NN_FC%num_layers = num_layers
+    Rad_NN_FC%num_hid_nodes = num_hid_nodes
+    allocate(Rad_NN_FC%Layers(num_layers))
+    do ilayer = 1, num_layers
+        allocate(Rad_NN_FC%Layers(ilayer)%weight(num_hid_nodes,num_hid_nodes))
+        allocate(Rad_NN_FC%Layers(ilayer)%bias(num_hid_nodes))
     end do
 end subroutine nn_init
 
-subroutine nn_pred_1d(x,y) 
+subroutine nn_pred_1d(x,y)
     real, dimension(:), intent(in) :: x
     real, dimension(:), intent(inout) :: y
-    integer::ilayer 
-     
+    integer::ilayer
+
     do ilayer = 1, Rad_NN_FC%num_layers
         y = matmul(x,Rad_NN_FC%Layers(ilayer)%weight)+ Rad_NN_FC%Layers(ilayer)%bias
         y = NN_activ(y)
@@ -49,29 +50,37 @@ real elemental function NN_activ(x)
     ! tanh
     ! NN_activ = tanh(x)
 end function NN_activ
- 
+
 
 end module test_mod
 Program test
 
 use test_mod
 implicit none
-real :: a(256,10000)  
+integer :: num_layers, num_hid_nodes
+real, allocatable, dimension(:,:) :: a
 real, allocatable, dimension(:)::c
 integer:: i,j, ilayer
-real :: start, finish 
-a = 2.431452 
+real :: start, finish
+num_layers = 5
+num_hid_nodes = 256
+!num_layers = 10
+!num_hid_nodes = 128
+!num_hid_nodes = 512
+!num_hid_nodes = 1024
+allocate(a(num_hid_nodes, int(1e5)))
+a = 2.431452
 
-call nn_init()
+
+call nn_init(num_layers, num_hid_nodes)
 ! allocate(c(size(a,1)))
 ! call nn_pred_1d(a(:,1),c)
 call cpu_time(start)
-do j = 1, int(1e4) 
+do j = 1, size(a,2)
     allocate(c(size(a,1)))
     call nn_pred_1d(a(:,j),c)
-    deallocate(c) 
+    deallocate(c)
 end do
 call cpu_time(finish)
-print '("Time = ",f6.3," seconds.")',finish-start   
+print '("Time = ",f6.3," seconds.")',finish-start
 End Program test
-
