c46991ef07ec3e9931fc22b3bcde46bb381c4e44
Chenggong Wang <c.wang@princeton.edu>
2023-02-16 14:32:07 -0500

    add comments

--- fortran_test/NN_speed2.f90
+++ fortran_test/NN_speed2.f90
@@ -1,10 +1,12 @@
 module test_mod
 implicit none
+! type for NN layer
 public NN_Linear_layer_type
 type :: NN_Linear_layer_type
     real, dimension(:,:), pointer :: weight=>NULL()
     real, dimension(:),   pointer :: bias=>NULL()
 end type NN_Linear_layer_type
+! type for NN structure
 public NN_FC_type
 type :: NN_FC_type
     integer :: num_hid_nodes
@@ -15,24 +17,26 @@ end type NN_FC_type
 type(NN_FC_type)   :: Rad_NN_FC
 
 contains
-
+! initialize NN 
 subroutine nn_init(num_layers, num_hid_nodes)
     integer, intent(in) :: num_layers, num_hid_nodes
     integer :: ilayer
     Rad_NN_FC%num_layers = num_layers
     Rad_NN_FC%num_hid_nodes = num_hid_nodes
     allocate(Rad_NN_FC%Layers(num_layers))
+    ! init each lay
     do ilayer = 1, num_layers
         allocate(Rad_NN_FC%Layers(ilayer)%weight(num_hid_nodes,num_hid_nodes))
         allocate(Rad_NN_FC%Layers(ilayer)%bias(num_hid_nodes))
     end do
 end subroutine nn_init
 
+! run NN with one input (one column)
 subroutine nn_pred_1d(x,y)
     real, dimension(:), intent(in) :: x
     real, dimension(:), intent(inout) :: y
     integer::ilayer
-
+    ! num_layers matmul called
     do ilayer = 1, Rad_NN_FC%num_layers
         y = matmul(x,Rad_NN_FC%Layers(ilayer)%weight)+ Rad_NN_FC%Layers(ilayer)%bias
         y = NN_activ(y)
@@ -62,19 +66,15 @@ real, allocatable, dimension(:,:) :: a
 real, allocatable, dimension(:)::c
 integer:: i,j, ilayer
 real :: start, finish
+
 num_layers = 5
 num_hid_nodes = 256
-!num_layers = 10
-!num_hid_nodes = 128
-!num_hid_nodes = 512
-!num_hid_nodes = 1024
+call nn_init(num_layers, num_hid_nodes)
+
+! dummpy input data with 1e5 columns
 allocate(a(num_hid_nodes, int(1e5)))
 a = 2.431452
 
-
-call nn_init(num_layers, num_hid_nodes)
-! allocate(c(size(a,1)))
-! call nn_pred_1d(a(:,1),c)
 call cpu_time(start)
 do j = 1, size(a,2)
     allocate(c(size(a,1)))
@@ -82,5 +82,6 @@ do j = 1, size(a,2)
     deallocate(c)
 end do
 call cpu_time(finish)
+
 print '("Time = ",f6.3," seconds.")',finish-start
 End Program test
